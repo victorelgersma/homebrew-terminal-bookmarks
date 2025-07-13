8919d6305d0922b64e5d87233d6ce8131910a03e
Victor Elgersma <victor.elgersma41@thg.com>
2025-07-13 09:23:08 +0100

    bump to 0.1.4

--- bm.rb
+++ bm.rb
@@ -1,9 +1,9 @@
 class Bm < Formula
   desc "Terminal bookmarks - the last bookmarking app you'll need"
   homepage "https://github.com/victorelgersma/terminal-bookmarks"
-  url "https://github.com/victorelgersma/terminal-bookmarks/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "cf833e822c670445cfb277cbc0990b3045bbaf9562e8b9f157020530820354a6"
-  version "0.1.3"
+  url "https://github.com/victorelgersma/terminal-bookmarks/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "17b8bf5da68b313ccfed906a3b1d542fc07590417787d8d25dcf4ab98a42e352"
+  version "0.1.4"
   license "MIT"
 
   def install
