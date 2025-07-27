c3ff46bc4b2a7a29cd139136f45640217907cb0b
Victor Elgersma <victorelgersma@Victors-MacBook-Pro.local>
2025-07-27 22:17:22 +0100

    update sha

--- bm.rb
+++ bm.rb
@@ -2,7 +2,7 @@ class Bm < Formula
   desc "Terminal bookmarks - the last bookmarking app you'll need"
   homepage "https://github.com/victorelgersma/terminal-bookmarks"
   url "https://github.com/victorelgersma/terminal-bookmarks/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "62b03c5d188e9636a80511cbe743e30c636b6ef26ae7fb4b81227db02ca0780c"
+  sha256 "002fba5f1f06dc792f97a918c254c765deaa3b96c3f80862e5411fbfab75bbf3"
   version "0.1.5"
   license "MIT"
 
